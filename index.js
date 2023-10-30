c7c970d98325f17130e2e26c79f72d1ee08617b7
Dimuthu Abeysinghe <41988837+Dimuthu7@users.noreply.github.com>
2023-10-30 07:45:31 +0530

    Add get all users query

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const {  } = require('graphql');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLList, GraphQLSchema } = require('graphql');
 
 const app = express();
+
+// sample data
+let usersList = [
+    {id: "1", name: "John", email: "john@example.com"},
+    {id: "2", name: "James", email: "james@example.com"},
+    {id: "3", name: "Michael", email: "michael@example.com"},
+]
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQuery",
+    fields: {
+        // to get all users
+        users: {
+            type: new GraphQLList(UserType),
+            resolve() {
+                return usersList;
+            }
+        },
+    }
+});
+
+const schema = new GraphQLSchema({query: RootQuery});
+
+app.use('/graphql', graphqlHTTP({schema, graphiql: true}));
 
 app.listen(5000, () => {
     console.log('Server Running');
